0e9b2b52e63236f6bd412b8902b6f1caae185a13
Jay Asbury <vbjay.net@gmail.com>
2013-03-22 11:24:59 -0400

    Added descriptions to setup push script.

--- GettingStarted/setupPush.sh
+++ GettingStarted/setupPush.sh
@@ -2,8 +2,9 @@
 
 #This script converts this repo to allow the repo to be pushed to github.
 
+echo "Configuring Portable Git repo push url with ssh url."
 git remote set-url --push origin git@github.com:vbjay/Portable-Git.git
 
 cd /share/GitExtensions/
-
+echo "Configuring Git Extensions repo push url with ssh url."
 git remote set-url --push origin git@github.com:vbjay/Git-Extensions.git
