58bb5fe3c9aac0557b29566ea9957baefe1f8bd0
Jay Asbury <vbjay.net@gmail.com>
2013-04-02 18:17:40 -0400

    Revert "Fix remote already exists error."
    This reverts commit cf07af653a85416ae85c9496cd6591885066521d.

--- GettingStarted/SetupSrcRepos.sh
+++ GettingStarted/SetupSrcRepos.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cd / &&
-git submodule update --init --recursive share/src/GitExtensions &&
-cd share/src/GitExtensions &&
-#git remote add upstream https://github.com/gitextensions/gitextensions.git &&
-git checkout master &&
+cd /
+git submodule update --init --recursive share/src/GitExtensions
+
+cd share/src/GitExtensions
+git checkout master
+git remote add upstream https://github.com/gitextensions/gitextensions.git
 git fetch upstream
 
 
