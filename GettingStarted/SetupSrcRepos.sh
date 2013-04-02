cf07af653a85416ae85c9496cd6591885066521d
Jay Asbury <vbjay.net@gmail.com>
2013-04-02 18:11:42 -0400

    Fix remote already exists error.

--- GettingStarted/SetupSrcRepos.sh
+++ GettingStarted/SetupSrcRepos.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-cd /
-git submodule update --init --recursive share/src/GitExtensions
-
-cd share/src/GitExtensions
-git checkout master
-git remote add upstream https://github.com/gitextensions/gitextensions.git
+cd / &&
+git submodule update --init --recursive share/src/GitExtensions &&
+cd share/src/GitExtensions &&
+#git remote add upstream https://github.com/gitextensions/gitextensions.git &&
+git checkout master &&
 git fetch upstream
 
 
