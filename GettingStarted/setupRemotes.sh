053e556a8fa169cdd2aa23c6d4fe34b8dde0bab2
Jay Asbury <vbjay.net@gmail.com>
2013-04-29 18:27:55 -0400

    Avoid error messages in setupremotes script.

--- GettingStarted/setupRemotes.sh
+++ GettingStarted/setupRemotes.sh
@@ -14,25 +14,40 @@ git remote set-url --push origin git@github.com:vbjay/Git-Extensions.git
 if [ -e "/share/src/GitExtensions/.git" ]; then
 	cd /share/src/GitExtensions/
 	echo "Configuring Git Extensions Src repo urls."
-	git remote set-url origin git://github.com/vbjay/gitextensions.git
+	echo "Removing remotes"
+	git remote remove origin
+	git remote remove upstream
+	echo "Adding remotes"
+	git remote add origin git://github.com/vbjay/gitextensions.git
 	git remote set-url --push origin git@github.com:vbjay/gitextensions.git
-	git remote set-url upstream	git://github.com/gitextensions/gitextensions.git 
+	git remote add upstream	git://github.com/gitextensions/gitextensions.git 
 	git remote set-url --push upstream	git://github.com/gitextensions/gitextensions.git
+	
 	
 	if [ -e "/share/src/GitExtensions/GitExtensionsDoc/.git" ]; then
 		echo "Configuring Git Extensions Doc repo urls."
 		cd /share/src/GitExtensions/GitExtensionsDoc/
-		git remote set-url origin	git://github.com/vbjay/GitExtensionsDoc.git 
+		echo "Removing remotes"
+		git remote remove origin
+		git remote remove upstream
+		echo "Adding remotes"
+		git remote add origin	git://github.com/vbjay/GitExtensionsDoc.git 
 		git remote set-url --push origin	git@github.com:vbjay/GitExtensionsDoc.git 
-		git remote set-url upstream	git://github.com/gitextensions/GitExtensionsDoc.git 
+		git remote add upstream	git://github.com/gitextensions/GitExtensionsDoc.git 
 		git remote set-url --push upstream	git://github.com/gitextensions/GitExtensionsDoc.git 
 	fi
 	if [ -e "/share/src/GitExtensions/GitExtensionsTest/.git" ]; then
 		echo "Configuring Git Extensions Test repo urls."
 		cd /share/src/GitExtensions/GitExtensionsTest/
-		git remote set-url origin	git://github.com/vbjay/gitextensionstest.git 
+		echo "Removing remotes"
+		git remote remove origin
+		git remote remove upstream
+		echo "Adding remotes"
+		git remote add origin	git://github.com/vbjay/gitextensionstest.git 
 		git remote set-url --push origin	git@github.com:vbjay/gitextensionstest.git 
-		git remote set-url upstream	git://github.com/gitextensions/gitextensionstest.git 
+		git remote add upstream	git://github.com/gitextensions/gitextensionstest.git 
 		git remote set-url --push upstream	git://github.com/gitextensions/gitextensionstest.git 
 	fi
 fi
+cd /
+git submodule foreach --recursive git remote -v update
