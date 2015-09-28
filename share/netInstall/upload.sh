a2351f881629d8500780c2dfb5854053d7548304
Jay Asbury <vbjay.net@gmail.com>
2015-09-28 16:01:07 -0400

    Update Portable GIT to 2.5.3

--- share/netInstall/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-filename=$(basename "$1")
-read -r -p "Please enter the username to login to vbjaysolutions.com." lname
-
-
-scp "$1" $lname@vbjaysolutions.com:public_html/vbjaysolutions.com/git/$filename
